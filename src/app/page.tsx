ea617c1532b3f902d104a08a7e1ed9ee012ea2a8
cheng01315 <58205298+cheng01315@users.noreply.github.com>
2025-03-02 02:28:12 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,9 +103,9 @@ function SearchParamsComponent() {
 
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      
-      <h1 className="text-4xl font-bold text-center mt-7 mb-2">欢迎来到喵喵工具导航</h1>
-      <p className="text-xl text-gray-600 text-center mt-0 mb-2">在这里，你将找到高效、实用的工具，简单易用，轻松搞定一切需求！</p>
+
+      <h1 className="text-4xl font-bold text-center mt-8 mb-4">欢迎来到喵喵工具导航</h1>
+      <p className="text-xl text-gray-600 text-center my-2">在这里，你将找到高效、实用的工具，简单易用，轻松搞定一切需求！</p>
   
       <TopBanner/>
       <div className="flex flex-1">
